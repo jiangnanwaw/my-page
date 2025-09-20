faa0de1d83150339c28558e518345fcdc4a84fe6
jiangnanwaw <471010996@qq.com>
2025-09-20 13:24:47 +0800

    Delete my-page directory

--- my-page/deepseek_javascript_20250805_e0e1c2.js
+++
@@ -1,4 +0,0 @@
-const year = document.getElementById('year-select').value;
-const month = document.getElementById('month-select').value;
-const fileName = `${year}${month}.html`;
-const fileUrl = `https://jiangnanwaw.github.io/my-page/${fileName}`;
